301b89a86e36a5a411c86fa7b7b40092b64f3933
Mainak Das <mainak97@outlook.com>
2022-10-25 21:44:44 +0530

    updated scd 2 logic, incomplete 2

--- scd-2.py
+++ scd-2.py
@@ -14,15 +14,26 @@ print(today)
 # dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 # For Load 1 (Day 1)
 df = pd.read_csv('dataset/data-1.csv')
-df_scd2 = df
-df_scd2['EFF_DATE'] = today
-df_scd2['END_DATE'] = MAX_END_DATE
-print(df_scd2)
-df_scd2.to_csv('output/scd-1/load-1.csv', index=False)
+df_hist = df
+df_hist['EFF_DATE'] = today
+df_hist['END_DATE'] = MAX_END_DATE
+print(df_hist)
+df_hist.to_csv('output/scd-1/load-1.csv', index=False)
 
-# # For Load 2 (Day 2)
-# df = pd.read_csv('dataset/data-2.csv')
-# df_scd1 = df
+# For Load 2 (Day 2)
+df = pd.read_csv('dataset/data-2.csv')
+df_temp = df_hist
+
+PRIMARY_KEY = 'ID'
+COLUMNS = df.columns
+k=0
+print((len(df.index),len(df_temp.index)))
+LENGTH = max(len(df.index),len(df_temp.index))
+result_1 = [i for i in df.iloc[1]]
+result_2 = [i for i in df_temp.iloc[1][:-2]]
+
+print(result_1,result_2)
+
 # df_scd1.to_csv('output/scd-1/load-2.csv', index=False)
 
 # print(df_scd1)
