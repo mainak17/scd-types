8055f2ec9c60265ce98914a8c806992062bcf4be
Mainak Das <mainak97@outlook.com>
2022-10-25 17:57:13 +0530

    updated scd 1 logic

--- scd-1.py
+++ scd-1.py
@@ -2,7 +2,14 @@
 
 import pandas as pd
 
+# For Load 1 (Day 1)
 df = pd.read_csv('dataset/data-1.csv')
-df_scd0 = df
-df_scd0.to_csv('output/scd-1.csv', index=False)
-print(df_scd0)
+df_scd1 = df
+df_scd1.to_csv('output/scd-1/load-1.csv', index=False)
+
+# For Load 2 (Day 2)
+df = pd.read_csv('dataset/data-2.csv')
+df_scd1 = df
+df_scd1.to_csv('output/scd-1/load-2.csv', index=False)
+
+print(df_scd1)
